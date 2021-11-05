2b47d734c5f61991691521dd7150add80774e677
diacrami <diacrami@espol.edu.ec>
2021-11-04 19:01:11 -0500

    validar cédula empresa

--- src/app/components/empresa/registro-full-empresa/registro-full-empresa.component.ts
+++ src/app/components/empresa/registro-full-empresa/registro-full-empresa.component.ts
@@ -29,6 +29,7 @@ export class RegistroFullEmpresaComponent implements OnInit {
   empresas: any []=[];
   new_empresa: any []=[];
   hide: boolean = true;
+  public validador= true; 
 
   constructor(
     private _tipodocumentoService: TipodocumentoService,
@@ -138,5 +139,58 @@ export class RegistroFullEmpresaComponent implements OnInit {
   show() {
     this.hide = !this.hide;
   }
+
+
+
+  //esta es la variable de validación
+  validadorDeCedula() {
+    let cedulaCorrecta = false;
+    console.log("entró al validador")
+    let cedula=this.formEmpresa.controls['ruc_cedula'].value
+    console.log(cedula.length)
+    if (cedula.length == 10)
+    {   
+        console.log("igual a 10")
+        let tercerDigito = parseInt(cedula.substring(2, 3));
+        if (tercerDigito < 6) {
+          console.log("menor a 6")
+        
+            // El ultimo digito se lo considera dígito verificador
+            let coefValCedula = [2, 1, 2, 1, 2, 1, 2, 1, 2];       
+            let verificador = parseInt(cedula.substring(9, 10));
+            let suma:number = 0;
+            let digito:number = 0;
+            for (let i = 0; i < (cedula.length - 1); i++) {
+                digito = parseInt(cedula.substring(i, i + 1)) * coefValCedula[i];      
+                suma += ((parseInt((digito % 10)+'') + (parseInt((digito / 10)+''))));
+          //      console.log(suma+" suma"+coefValCedula[i]); 
+            }
+            
+            suma= Math.round(suma);
+          
+          //  console.log(verificador);
+          //  console.log(suma);
+          //  console.log(digito);
+  
+            if ((Math.round(suma % 10) == 0) && (Math.round(suma % 10)== verificador)) {
+                cedulaCorrecta = true;
+            } else if ((10 - (Math.round(suma % 10))) == verificador) {
+                cedulaCorrecta = true;
+            } else {
+                cedulaCorrecta = false;
+            }
+        } else {
+            cedulaCorrecta = false;
+        }
+    } else {
+        cedulaCorrecta = false;
+    }
+  
+  
+  this.validador= cedulaCorrecta;
+  
+    
+  }
+
   
 }
