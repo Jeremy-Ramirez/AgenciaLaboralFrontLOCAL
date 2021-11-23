52f57981b313dce0a97651286e2102d2fe99ffc0
Jeremy-Ramirez <66746920+Jeremy-Ramirez@users.noreply.github.com>
2021-11-23 15:36:45 -0500

    Creacion select

--- src/app/components/representante/filtro/filtro.component.ts
+++ src/app/components/representante/filtro/filtro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-filtro',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filtro.component.css']
 })
 export class FiltroComponent implements OnInit {
+  miFormulario: FormGroup= this.fb.group({
+    provincia:['',Validators.required]
+  })
 
-  constructor() { }
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+  solicitar(){
+    console.log(this.miFormulario.value);
+  }
 }
