1f61dcdd869d30f8098b2224dc7433d37f21b977
diacrami <diacrami@espol.edu.ec>
2021-11-24 19:24:40 -0500

    logout admin

--- src/app/components/administrador/administrador-routing.module.ts
+++ src/app/components/administrador/administrador-routing.module.ts
@@ -34,7 +34,7 @@ const routes: Routes = [
           ]
         },
         { path: 'registroAdministrador', component: RegistroAdministradorComponent },
-        { path: '**', redirectTo: 'registroAdministrador' }
+        { path: '**', redirectTo: 'sesionAdministrador/perfilAdministrador' }
       ]
     }
   ];
--- src/app/components/administrador/menu-lateral/menu-lateral.component.ts
+++ src/app/components/administrador/menu-lateral/menu-lateral.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { Emitters } from '../clases/emitters';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-menu-lateral',
@@ -11,6 +13,7 @@ import { map, shareReplay } from 'rxjs/operators';
 export class MenuLateralComponent {
   aspirantesAceptados = false;
   sesionAspirante = false;
+  authenticated = false;
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
@@ -18,15 +21,28 @@ export class MenuLateralComponent {
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+    constructor(private breakpointObserver: BreakpointObserver, private http: HttpClient) {}
+  
+    ngOnInit(): void {
+      Emitters.authEmitter.subscribe(
+        (auth: boolean) => {
+          this.authenticated = auth;
+        }
+      );
+    }
+  
+    logout(): void {
+      this.http.post('http://localhost:8000/api/logoutusuario/', {}, {withCredentials: true})
+        .subscribe(() => this.authenticated = false);
+    }
 
-  showAspirantesAceptados(): void {
+  /*showAspirantesAceptados(): void {
     this.aspirantesAceptados = true;
     this.sesionAspirante = false;
   }
   showSesionAspirante(): void {
     this.sesionAspirante = true;
     this.aspirantesAceptados = false;
-  }
+  }*/
 
 }
